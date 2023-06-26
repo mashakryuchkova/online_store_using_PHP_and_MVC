9cd5a8384875131cdfb7b576bb1c99a0d5e37432
mashakryuchkova <61075979+mashakryuchkova@users.noreply.github.com>
2023-06-26 19:12:42 +0300

    Update Config.php

--- diplom/app/libs/Config.php
+++ diplom/app/libs/Config.php
@@ -1,16 +1,16 @@
 <?php
-    define("MAIN_PREFIX", "/kryuchkova/diplom");
-    define("TEMPLANE_PATH", "/kryuchkova/diplom/app/views");
+    define("MAIN_PREFIX", "/your_path");
+    define("TEMPLANE_PATH", "/your_path/app/views");
     define("DIR_PATH_APP", __DIR__.'/..');
-    define("BASE_DIR_AJAX", "/kryuchkova/diplom/app/ajax");
+    define("BASE_DIR_AJAX", "/your_path/app/ajax");
 
-    define("DBLH", '94.228.124.234');
-    define("DBNAME", 'mkruchkova_diplom');
-    define("DBUSER", 'mkruchkova');
-    define("DBPASS", 'x40*6QVDZE');
+    define("DBLH", 'your_localhost');
+    define("DBNAME", 'your_name');
+    define("DBUSER", 'your_user');
+    define("DBPASS", 'your_password');
     
     define("ADMIN_ROLE", "1");
-    define("ADMIN_PREFIX", "/kryuchkova/diplom/admin");
-    define("TEMPLANE_ADMIN_PATH", "/kryuchkova/diplom/app/views/admin");
+    define("ADMIN_PREFIX", "/your_path/admin");
+    define("TEMPLANE_ADMIN_PATH", "/your_path/app/views/admin");
     
     define("MAX_UPLOAD_SIZE", "5242880");
